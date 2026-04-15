4886d1063f468481dc33672bb183d28d3939ea8f
a_sivaraman <a_sivaraman@apple.com>
2026-04-14 17:08:19 -0700

    T1: WAL mode in mdb_ops.open_db

--- music/mdb_ops.py
+++ music/mdb_ops.py
@@ -298,6 +298,8 @@ def open_db(path=None) -> sqlite3.Connection:
     conn = sqlite3.connect(path or DB_PATH)
     conn.row_factory = sqlite3.Row
     conn.execute('PRAGMA foreign_keys = ON')
+    conn.execute('PRAGMA journal_mode = WAL')
+    conn.execute('PRAGMA synchronous = NORMAL')
     return conn
 
 
